4f639f336d0adf92a6106c0640b9c2d18789ea6d
murilobispo <murilooliveirabispo90@gmail.com>
2025-12-30 03:09:02 -0300

    🚧 Testing geo APIs

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 import streamlit as st
+import pandas as pd
+import requests
+
+st.set_page_config(page_title="Weather App", layout="centered")
+
+def get_data(city):
+    url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=10&language=en&format=json"
+    response = requests.get(url).json()["results"]
+    df = pd.DataFrame(response)
+    df = df[["name", "country","country_code", "admin1", "latitude", "longitude","timezone","feature_code"]]
+
+    st.dataframe(df, use_container_width=True)
+
 
 city_input = st.text_input(label="city-input",
+                           key="city-input",
                            value=None,
                            label_visibility="hidden",
-                           placeholder="city")
+                           placeholder="city"
+                           )
+if city_input:
+    get_data(city_input)
+
+#metric = st.metric(label="Tempeture", 
+                #value="70 °F", )
